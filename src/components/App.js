45ff2528b236684206217f49db31de9f4c4e055e
RakeshDP <rakeshpawar1895.rp@gmail.com>
2023-09-11 00:15:46 +0530

    submitting code-Mon Sep 11 2023 00:15:45 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,62 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
+import '../styles/App.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            renderBall: false,
+            ballPosition: { left: "0px" }
+        };
+
+        // Bind event handlers to the class instance
+        this.buttonClickHandler = this.buttonClickHandler.bind(this);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+    }
+
+    // Event handler for the "Start" button click
+    buttonClickHandler() {
+        this.setState({ renderBall: true });
+
+        // Add the event listener for the right arrow key when the game starts
+        document.addEventListener("keydown", this.handleKeyDown);
+    }
+
+    // Event handler for the right arrow key
+    handleKeyDown(event) {
+        if (event.key === "ArrowRight" || event.keyCode === 39) {
+            this.setState((prevState) => ({
+                ballPosition: {
+                    left: (parseInt(prevState.ballPosition.left) + 5) + "px",
+                },
+            }));
+        }
+    }
+
+    // Remove the event listener when the component unmounts
+    componentWillUnmount() {
+        document.removeEventListener("keydown", this.handleKeyDown);
+    }
+
+    render() {
+        return (
+            <div className="playground">
+                {this.state.renderBall ? (
+                    <div className="ball" style={this.state.ballPosition}></div>
+                ) : (
+                    <button className="start" onClick={this.buttonClickHandler}>
+                        Start
+                    </button>
+                )}
+            </div>
+        );
+    }
+}
+
+export default App;
+
+
+/*import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 class App extends Component {
@@ -40,3 +98,4 @@ class App extends Component {
 
 
 export default App;
+*/
